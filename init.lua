72a50a323c489488f288d16ba98da7b9d00c546d
bosapara <38753015+bosapara@users.noreply.github.com>
2019-06-29 21:12:30 +0400

    bucket adding

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ local v = {
 }
 for _, v in pairs(v) do
 
-	minetest.register_tool("lets_paint:"..v[1], {
+	minetest.register_tool("lets_paint:brush_"..v[1], {
 		description = "Brush "..v[2],
 		inventory_image = "brush_"..v[1]..".png",
 		range = 40,
@@ -30,7 +30,7 @@ for _, v in pairs(v) do
 
 			local name = placer:get_player_name()
 
-			if (npos.y < 400 or npos.y > 430) or (npos.x < 96 or npos.x > 120) or (npos.z < - 127 or npos.z > -103) then
+			if (npos.y < 400 or npos.y > 430 or npos.x < 91 or npos.x > 125 or npos.z < - 132 or npos.z > -98) then
 			minetest.chat_send_player(name, (minetest.colorize("red","<Mr.Bot> "..name..", you can't use brush tool here!")))
 			--placer:set_wielded_item("default:stick")
 			itemstack:take_item()
@@ -46,6 +46,35 @@ for _, v in pairs(v) do
 	
 	})
 	
+	minetest.register_tool("lets_paint:bucket_"..v[1], {
+		description = "Bucket "..v[2],
+		inventory_image = "bucket_"..v[1]..".png",
+		range = 40,
+
+		on_place = function(itemstack, placer, pointed_thing)
+		
+			local npos = pointed_thing.under
+			--local pos = placer:get_pos()
+
+			local name = placer:get_player_name()
+
+			if (npos.y < 400 or npos.y > 430 or npos.x < 91 or npos.x > 125 or npos.z < - 132 or npos.z > -98) then
+			minetest.chat_send_player(name, (minetest.colorize("red","<Mr.Bot> "..name..", you can't use brush tool here!")))
+			--placer:set_wielded_item("default:stick")
+			itemstack:take_item()
+			return itemstack
+			end
+			
+			local item = placer:get_wielded_item():to_string()
+		
+			minetest.show_formspec(placer:get_player_name(), "brush_form", "size[11.5,8] bgcolor[#08080877;false] image_button_exit[1,1.5;1.5,1.5;wool_green.png;green;] image_button_exit[3,1.5;1.5,1.5;wool_red.png;red;] image_button_exit[5,1.5;1.5,1.5;wool_blue.png;blue;] image_button_exit[7,1.5;1.5,1.5;wool_yellow.png;yellow;] image_button_exit[9,1.5;1.5,1.5;wool_violet.png;violet;] image_button_exit[1,3.5;1.5,1.5;wool_dark_green.png;dark_green;] image_button_exit[3,3.5;1.5,1.5;wool_pink.png;pink;] image_button_exit[5,3.5;1.5,1.5;wool_cyan.png;cyan;] image_button_exit[7,3.5;1.5,1.5;wool_magenta.png;magenta;] image_button_exit[9,3.5;1.5,1.5;wool_brown.png;brown;] image_button_exit[1,5.5;1.5,1.5;wool_orange.png;orange;] image_button_exit[3,5.5;1.5,1.5;wool_white.png;white;] image_button_exit[5,5.5;1.5,1.5;wool_dark_grey.png;dark_grey;] image_button_exit[7,5.5;1.5,1.5;wool_black.png;black;] image_button_exit[9,5.5;1.5,1.5;wool_grey.png;grey;]")					
+		
+			
+		end,
+	
+	})	
+	
+	
 	
 end
 
@@ -57,11 +86,15 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 
 		for _, v in pairs(v) do
 	
-			if fields[v[1]] and string.match(item, "lets_paint:") then
+			if fields[v[1]] and string.match(item, "lets_paint:brush_") then
 
-				player:set_wielded_item("lets_paint:"..v[1])
+				player:set_wielded_item("lets_paint:brush_"..v[1])
 
-			end	
+			elseif fields[v[1]] and string.match(item, "lets_paint:bucket_") then
+			
+				player:set_wielded_item("lets_paint:bucket_"..v[1])
+			
+			end
 								
 		end
 
@@ -77,21 +110,51 @@ minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 	
 	local npos = pointed_thing.under
 	
-	if (npos.y < 400 or npos.y > 430) or (npos.x < 96 or npos.x > 120) or (npos.z < - 127 or npos.z > -103) and string.match(item, "lets_paint:") then
+	
+	local nn = minetest.get_node(npos).name	
+	
+	
+	if (npos.y < 400 or npos.y > 430 or npos.x < 91 or npos.x > 125 or npos.z < - 132 or npos.z > -98) and string.match(item, "lets_paint:") then
 		
 		puncher:set_wielded_item("")
 		minetest.chat_send_player(name, (minetest.colorize("red","<Mr.Bot> "..name..", you can't use brush tool here!")))
-	else
+		
+	elseif string.match(item, "lets_paint:brush_") then
 	
 		for _, v in pairs(v) do
 
-			if puncher:get_wielded_item():get_name() == "lets_paint:"..v[1] and node.name ~= "air" and string.match(node.name, "wool:") and not string.match(node.name, "carpet") then
+			if puncher:get_wielded_item():get_name() == "lets_paint:brush_"..v[1] and node.name ~= "air" and string.match(node.name, "wool:") and not string.match(node.name, "carpet") then
 			
 				minetest.set_node(pos, {name="wool:"..v[1]})
 										
 			end
 								
 		end	
+		
+		
+	elseif string.match(item, "lets_paint:bucket_") then
+
+
+		for _, v in pairs(v) do
+
+			if puncher:get_wielded_item():get_name() == "lets_paint:bucket_"..v[1] and node.name ~= "air" and string.match(node.name, "wool:") and not string.match(node.name, "carpet") then
+
+
+				local pos1 = {x=npos.x-15, y=pos.y - 15 , z=npos.z-15}
+				local pos2 = {x=npos.x+15, y=pos.y + 15, z=npos.z+15}
+				for _,pos in ipairs(minetest.find_nodes_in_area(pos1, pos2, {"group:wool"})) do
+					minetest.set_node(pos, {name="wool:"..v[1]})
+				end
+												
+			end
+								
+		end	
+	
+
 	end
 
+
+
 end)
+
+
